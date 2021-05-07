1c30159f5e60ad2c5aafbce0d2f2ca0baae33fb5
shreyaskn72 <60369972+shreyaskn72@users.noreply.github.com>
2021-05-07 12:50:35 +0530

    Create hello.py

---
+++ 0-hello-world/hello.py
@@ -0,0 +1,17 @@
+"""
+Program illustrates to begin using flask for rest api development
+
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def hello_world():
+    return 'Hello, From Flask!'
+    #return 'Hello, From Django!'
+
+if __name__== '__main__':
+    app.run()
