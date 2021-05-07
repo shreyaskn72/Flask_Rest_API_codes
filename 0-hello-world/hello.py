2b435e97bfdf782a99d1eaf27bb2f3b4fd8153d4
shreyaskn72 <60369972+shreyaskn72@users.noreply.github.com>
2021-05-07 13:00:48 +0530

    Update hello.py

--- 0-hello-world/hello.py
+++ 0-hello-world/hello.py
@@ -10,8 +10,8 @@ app = Flask(__name__)
 @app.route('/')
 
 def hello_world():
-    return 'Hello, From Flask!'
-    #return 'Hello, From Django!'
+    return 'Hello, From Shreyas!'
+
 
 if __name__== '__main__':
     app.run()
